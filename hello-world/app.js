8b7dfc517acd7572cec41d0d4e55ceed741bf608
Lorenzo Dessimoni <lorenzo.dessimoni@corley.it>
2023-05-05 15:11:44 +0200

    test

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,36 +1,15 @@
-const { QuickSightClient, GenerateEmbedUrlForRegisteredUserCommand } = require("@aws-sdk/client-quicksight");
+const { QuickSightClient, GetDashboardEmbedUrlCommand } = require("@aws-sdk/client-quicksight");
 
 exports.lambdaHandler = async (event, context) => {
     try {
-        const client = new QuickSightClient({ region: "eu-central-1" });
-        const input = {
+        const client = new QuickSightClient();
+        const command = new GetDashboardEmbedUrlCommand({
             AwsAccountId: "327465495978", // required
+            DashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9", // required
+            IdentityType: "IAM", // required
             SessionLifetimeInMinutes: 60,
-            UserArn: "arn:aws:lambda:eu-central-1:327465495978:function:test-quicksight-HelloWorldFunction-mYsxkQIFJQnZ", // required
-            ExperienceConfiguration: {
-                Dashboard: {
-                    InitialDashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9", // required
-                    FeatureConfigurations: {
-                        StatePersistence: {
-                            Enabled: false, // required
-                        },
-                        Bookmarks: {
-                            Enabled: false, // required
-                        },
-                    },
-                },
-                DashboardVisual: {
-                    // RegisteredUserDashboardVisualEmbeddingConfiguration
-                    InitialDashboardVisualId: {
-                        // DashboardVisualId
-                        DashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9", // required
-                        SheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788", // required
-                        VisualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_88f62564-b9b9-43e4-95fa-91f52f79386d", // required
-                    },
-                },
-            },
-        };
-        const command = new GenerateEmbedUrlForRegisteredUserCommand(input);
+            UserArn: "arn:aws:lambda:eu-central-1:327465495978:function:test-quicksight-HelloWorldFunction-mYsxkQIFJQnZ"
+        });
         const response = await client.send(command);
         return { statusCode: 200, body: JSON.stringify(response) };
     } catch (err) {
