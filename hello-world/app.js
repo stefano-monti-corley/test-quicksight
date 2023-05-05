36c390a4d96314e7392cf91a216cc39df1481805
Lorenzo Dessimoni <lorenzo.dessimoni@corley.it>
2023-05-05 17:59:39 +0200

    Create test env

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,17 +1,25 @@
-const { QuickSightClient, GetDashboardEmbedUrlCommand } = require("@aws-sdk/client-quicksight");
+const { QuickSightClient, GenerateEmbedUrlForRegisteredUserCommand } = require("@aws-sdk/client-quicksight");
 
 exports.lambdaHandler = async (event, context) => {
     try {
-        const client = new QuickSightClient();
-        const command = new GetDashboardEmbedUrlCommand({
-            AwsAccountId: "327465495978", // required
-            DashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9", // required
-            IdentityType: "IAM", // required
+        const client = new QuickSightClient({ region: "eu-central-1" });
+
+        const command = new GenerateEmbedUrlForRegisteredUserCommand({
+            AwsAccountId: "327465495978",
             SessionLifetimeInMinutes: 60,
-            UserArn: "arn:aws:lambda:eu-central-1:327465495978:function:test-quicksight-HelloWorldFunction-mYsxkQIFJQnZ"
+            UserArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
+            ExperienceConfiguration: {
+                DashboardVisual: {
+                    InitialDashboardVisualId: {
+                        DashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
+                        SheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
+                        VisualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_88f62564-b9b9-43e4-95fa-91f52f79386d",
+                    },
+                },
+            }
         });
         const response = await client.send(command);
-        return { statusCode: 200, body: JSON.stringify(response) };
+        return { statusCode: 200, body: response };
     } catch (err) {
         console.log(err);
         return err;
