9d2444c7d8f13c692c771353b4f306dc5ec299dd
Lorenzo Dessimoni <lorenzo.dessimoni@corley.it>
2023-05-05 15:04:08 +0200

    Remove credentials

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,14 +2,7 @@ const { QuickSightClient, GenerateEmbedUrlForRegisteredUserCommand } = require("
 
 exports.lambdaHandler = async (event, context) => {
     try {
-        const client = new QuickSightClient({
-            region: "eu-central-1",
-            credentials: {
-                accessKeyId: "ASIAUYPTW5WVFR3OASRS",
-                secretAccessKey: "fRD/zniVT/j4Jhgc1IXtZF42FnJb/DOgljSBeqYh",
-                sessionToken: "IQoJb3JpZ2luX2VjEPv//////////wEaDGV1LWNlbnRyYWwtMSJHMEUCIQCOCdoY+N9oRzYwbL7zpQhRabJI/EeXJhsE/rbmRtpylwIgZukGqKBGjTVHOCPAskd5dFSxKsSj2U40EqVkfTG531YqlQMIFBACGgwzMjc0NjU0OTU5NzgiDLQHDK1ktGQw2JtCoSryAp75VIZxermCn3j6DT0EzBzIqpHSuj9nHtTcW1LjEhdqZElrj+SBvUjNuFsl1jURIKrtlo8nMhB4eR67D+Fxd7k/W1yIbs+wxtKG3uIufu6RBrYCnnv+MBd06eMeAKjYJIyzXDVfsXSSetwohiwkng/R/bDb3DPvkiNKfJIavhiKyB42ODJrV5NVybeh5zHtU1UhHTStYcDchbgvT2wRO4ptyDSvbJPTNnWaJul/nWxx7O4C79/D+86movbaL6W5fh49AJlsC5QJb+s+vsGOaFrcX+w+BRjpKMtxem5b5EXKYUijXLSSXN+T/FR1PzC9rkhThw5hY5ju7yHA+HbL82gAVJQNlDL1WsCe/wA5nHZga+wKdYwT+gm3dUVBEw6VfpwkyuV72ygU0XrJ4qbaFmIro3MmNab1okmMYN/iOXEgE+wvBHE7672ucQk1ezLg95SrqqopDhH/fXN0FPr5xpjjtgEolpEb7XCGwwtxhstgRMkwirrTogY6pgED9lyMb16p/aisW8tgscM2Dblb3UpuzXJNJC5aOhBEd7og6/YFt+z1ZjJEZDYfOvLzSn69wdYBreLs4QBv0gjnHC05yhf4dK7CEatQiMY3V/QOFJxtlZ47h0fRffIRlc98xu7ScgkJ80WU76HSSZLp3VW9WTfDuMeHG3bmNaUHPURtNQmBbHQKe7+IEplBbYISXo7V6PmkIFXlOagA11svb9nhirRD"
-            }
-        });
+        const client = new QuickSightClient({ region: "eu-central-1" });
         const input = {
             AwsAccountId: "327465495978", // required
             SessionLifetimeInMinutes: 60,
