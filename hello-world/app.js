acabf1086c4b366f3f38c630639d1cc37ecc67ac
Lorenzo Dessimoni <lorenzo.dessimoni@corley.it>
2023-05-17 16:27:02 +0200

    Change lambda

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,139 +1,103 @@
 const { QuickSightClient, GenerateEmbedUrlForRegisteredUserCommand, DescribeUserCommand } = require("@aws-sdk/client-quicksight");
+const config = require("config");
 
-
-const client = new QuickSightClient({ region: "eu-central-1" }); // inizializzazione di un client va sempre messo fuori dal lambda handler per motivi di performance. https://docs.aws.amazon.com/lambda/latest/dg/best-practices.html
-
+const client = new QuickSightClient({ region: "eu-central-1" });
 exports.lambdaHandler = async (event, context) => {
-
-    
-
+    // const username = config.get("quicksightUsername");;
+    // const awsAccountId = config.get("awsAccountId");;
+    const awsAccountId = "327465495978";
     function getGraph({ dashboardId, sheetId, visualId, awsAccountId, userArn }) {
         const command = new GenerateEmbedUrlForRegisteredUserCommand({
             AwsAccountId: awsAccountId,
             SessionLifetimeInMinutes: 480,
             UserArn: userArn,
             ExperienceConfiguration: {
-                "Dashboard": { 
-                    "InitialDashboardId": dashboardId
+                DashboardVisual: {
+                    InitialDashboardVisualId: {
+                        DashboardId: dashboardId,
+                        SheetId: sheetId,
+                        VisualId: visualId,
+                    },
                 },
             }
         });
-
         return client.send(command);
     }
+
+    function getDashboard({ dashboardId, awsAccountId, userArn }) {
+        const command = new GenerateEmbedUrlForRegisteredUserCommand({
+            AwsAccountId: awsAccountId,
+            SessionLifetimeInMinutes: 480,
+            UserArn: userArn,
+            ExperienceConfiguration: {
+                Dashboard: {
+                    InitialDashboardId: dashboardId
+                },
+            }
+        });
+        return client.send(command);
+    }
+
     try {
-
-        const graph1 = await getGraph({
-            awsAccountId:"327465495978",
+        const graphMasterCommand = getGraph({
+            awsAccountId,
             userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-            dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-            sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-            visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_88f62564-b9b9-43e4-95fa-91f52f79386d",
+            dashboardId: "fe31ddde-1b16-45b5-a51b-f818982d26e9",
+            sheetId: "fe31ddde-1b16-45b5-a51b-f818982d26e9_2868bd74-6f0e-44c6-a58f-883c6a25d4a6",
+            visualId: "fe31ddde-1b16-45b5-a51b-f818982d26e9_1954359f-3941-44c0-9a5d-eb851e9a26ec",
+        });
+        const kpiMasterCommand = getGraph({
+            awsAccountId,
+            userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
+            dashboardId: "fe31ddde-1b16-45b5-a51b-f818982d26e9",
+            sheetId: "fe31ddde-1b16-45b5-a51b-f818982d26e9_2868bd74-6f0e-44c6-a58f-883c6a25d4a6",
+            visualId: "fe31ddde-1b16-45b5-a51b-f818982d26e9_3b9b1731-d739-4c56-b724-39434b1fbfa5",
+        });
+        const graphCakeCommand = getGraph({
+            awsAccountId,
+            userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
+            dashboardId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b",
+            sheetId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b_ba74e40a-874e-4c2f-9df1-368ba1b6cced",
+            visualId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b_1baa86fe-0780-4273-aab0-d89b6492b88e",
+        });
+        const graphBarCommand = getGraph({
+            awsAccountId,
+            userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
+            dashboardId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b",
+            sheetId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b_ba74e40a-874e-4c2f-9df1-368ba1b6cced",
+            visualId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b_6f6ee087-3608-4610-856e-d7d50a890212",
         });
 
-        const graph2 = getGraph({
-            awsAccountId:"327465495978",
+        const dashboardCommand = getDashboard({
+            awsAccountId,
             userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-            dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-            sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-            visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_9c1262e7-ddee-4470-b1dc-0804467c4652",
+            dashboardId: "f850a3d2-aa1e-4dab-9960-fd6038f1a77b"
         });
 
-        const graph3 = getGraph({
-            awsAccountId:"327465495978",
-            userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-            dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-            sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-            visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_3555aeee-1e84-44a4-b159-9c444eb9f8ee",
-        });
+        const [graphMaster, kpiMaster, dashboard, graphCake, graphBar] = await Promise.all([
+            graphMasterCommand, 
+            kpiMasterCommand, 
+            dashboardCommand, 
+            graphCakeCommand, 
+            graphBarCommand
+        ]);
 
+        const mapper = ({ EmbedUrl }) => EmbedUrl;
+        const urls = [graphMaster, kpiMaster].map(mapper);
 
-        return graph1;
+        return {
+            statusCode: 200,
+            body: {
+                urls,
+                dashboard: mapper(dashboard),
+                components: {
+                    cake: mapper(graphCake),
+                    bar: mapper(graphBar)
+                }
+            }
+        };
     } catch (err) {
         console.log(err);
         return err;
     }
 };
-
-
-
-// const { QuickSightClient, GenerateEmbedUrlForRegisteredUserCommand, DescribeUserCommand } = require("@aws-sdk/client-quicksight");
-// const config = require("config");
-
-// const client = new QuickSightClient({ region: "eu-central-1" });
-
-// exports.lambdaHandler = async (event, context) => {
-//     const username = config.get("quicksightUsername");;
-//     const awsAccountId = config.get("awsAccountId");;
-
-    
-
-//     function getGraph({ dashboardId, sheetId, visualId, awsAccountId, userArn }) {
-//         const command = new GenerateEmbedUrlForRegisteredUserCommand({
-//             AwsAccountId: awsAccountId,
-//             SessionLifetimeInMinutes: 480,
-//             UserArn: userArn,
-//             ExperienceConfiguration: {
-//                 DashboardVisual: {
-//                     InitialDashboardVisualId: {
-//                         DashboardId: dashboardId,
-//                         SheetId: sheetId,
-//                         VisualId: visualId,
-//                     },
-//                 },
-//             }
-//         });
-
-//         return client.send(command);
-//     }
-//     try {
-//         // const describeUserCommand = new DescribeUserCommand({
-//         //     AwsAccountId: awsAccountId,
-//         //     UserName: username,
-//         //     Namespace: "default"
-//         // });
-
-//         // const { User, $metadata } = await client.send(describeUserCommand);
-//         // if ($metadata.httpStatusCode !== 200) {
-//         //     throw new Error(`DescribeUser failed with http status code ${$metadata.httpStatusCode}`);
-//         // }
-
-        
-
-//         // if (!User.Arn || !User.Active) {
-//         //     throw new Error(`User with username ${username} not found.`);
-//         // }
-
-//         const graph1 = getGraph({
-//             awsAccountId,
-//             userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-//             dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-//             sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-//             visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_88f62564-b9b9-43e4-95fa-91f52f79386d",
-//         });
-
-//         const graph2 = getGraph({
-//             awsAccountId,
-//             userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-//             dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-//             sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-//             visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_9c1262e7-ddee-4470-b1dc-0804467c4652",
-//         });
-
-//         const graph3 = getGraph({
-//             awsAccountId,
-//             userArn: "arn:aws:quicksight:eu-central-1:327465495978:user/default/AWSReservedSSO_AdministratorAccess_aa0b2176933fbdd4/lorenzo.dessimoni@corley.it",
-//             dashboardId: "d3e64692-410a-4ea8-9af7-0f05daf561c9",
-//             sheetId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_385023b9-1ff7-4e4f-9118-ae4ca7c01788",
-//             visualId: "d3e64692-410a-4ea8-9af7-0f05daf561c9_3555aeee-1e84-44a4-b159-9c444eb9f8ee",
-//         });
-
-
-//         const responses = await Promise.all([graph1, graph2, graph3]);
-//         const urls = responses.map(({ EmbedUrl }) => EmbedUrl);
-//         return { statusCode: 200, body: { urls } };
-//     } catch (err) {
-//         console.log(err);
-//         return err;
-//     }
-// };
